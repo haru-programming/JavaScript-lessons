f03dae0c0f334af234c6b0040701f86022d5ebdf
haru-programming <h136479442@gmail.com>
2021-11-28 10:16:26 +0900

    wip: Error handling when an empty array is caught.

--- lesson11/index.js
+++ lesson11/index.js
@@ -1,4 +1,5 @@
 const ul = document.getElementById("js-lists");
+const div = document.getElementById("js-contents");
 
 function addLoading() {
     const li = document.createElement("li");
@@ -16,7 +17,8 @@ function removeLoading() {
 
 async function fetchData() {
     try{
-        const responseApi = await fetch("https://myjson.dit.upm.es/api/bins/7ctn");
+        // const responseApi = await fetch("https://myjson.dit.upm.es/api/bins/7ctn");
+        const responseApi = await fetch("https://myjson.dit.upm.es/api/bins/bu5z");//空の配列
         const json = await responseApi.json();
         return json.data;
     } catch(e) {
@@ -26,11 +28,21 @@ async function fetchData() {
 
 async function fetchListData() {
     addLoading();
+    const data = await fetchData();
+
     try {
-        return await fetchData();
-    } catch(e) {
+        if (data.length === 0) {
+            div.textContent = "エラー：データが存在しません";
+            throw new Error("データが空です");
+        }
+        return data;
+    } catch (e) {
         console.error(e.message);
+        if (data.length === !0) {
+            div.textContent = "エラー：データを取得できませんでした";
+        }
     } finally {
+        console.log(document.getElementById("js-loading"));
         removeLoading();
     }
 };
@@ -39,20 +51,22 @@ async function addList() {
     const fragment = document.createDocumentFragment();
     const values = await fetchListData();
 
-    values.forEach(value => {
-        const li = document.createElement("li");
-        const anchor = document.createElement("a");
-        const img = document.createElement("img");
-    
-        anchor.textContent = value.text;
-        anchor.href = `/${value.to}`;
-        img.src = value.img;
-        img.alt = value.alt;
-    
-        li.appendChild(anchor).insertAdjacentElement("afterbegin", img);
-        fragment.appendChild(li);
-    });
-    ul.appendChild(fragment);
+    if (values) {
+        values.forEach(value => {
+            const li = document.createElement("li");
+            const anchor = document.createElement("a");
+            const img = document.createElement("img");
+        
+            anchor.textContent = value.text;
+            anchor.href = `/${value.to}`;
+            img.src = value.img;
+            img.alt = value.alt;
+        
+            li.appendChild(anchor).insertAdjacentElement("afterbegin", img);
+            fragment.appendChild(li);
+        });
+        ul.appendChild(fragment);
+    }
 };
 
 addList();
