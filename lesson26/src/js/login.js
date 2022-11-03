b903da996a0d3bab5f14f5e7db84020b2f34315a
haru-programming <h136479442@gmail.com>
2022-11-03 22:11:59 +0900

    feat: Login Function

---
+++ lesson26/src/js/login.js
@@ -0,0 +1,70 @@
+const form = document.getElementById("js-form");
+const userIdOfInput = document.querySelector(".js-form-userid");
+const passwordOfInput = document.querySelector(".js-form-password");
+const formElements = [userIdOfInput,passwordOfInput];
+const submitButton = document.getElementById("js-submit-button");
+
+const isNotEmptyOfInput = target => target.value.trim() !== "";
+const isNotEmptyOfAllInput = () => {
+    let result = false;
+    formElements.forEach(element => {
+        element.value.trim() !== "" ? result = true : result = false;
+    })
+    return result;
+}
+
+const checkFormValidityToEnableSubmitButton = () => submitButton.disabled = isNotEmptyOfAllInput() ? false : true;
+
+formElements.forEach(element => {
+    element.addEventListener("blur", (e) => {
+        checkFormValidityInBlur(e.target);
+    });
+});
+
+const checkFormValidityInBlur = target => {
+    submitButton.disabled = true;
+
+    if (!isNotEmptyOfInput(target)) {
+        target.nextElementSibling.textContent = "入力してください";
+        return;
+    }
+
+    target.nextElementSibling.textContent = "";
+    checkFormValidityToEnableSubmitButton();
+};
+
+const getInputValueAndTryLogin = () => {
+    const inputsValues = {
+        userId: document.querySelector(".js-form-userid").value,
+        password: document.querySelector(".js-form-password").value
+    }
+    tryToLogin(inputsValues);
+}
+
+const tryToLogin = async(inputsValues) => {
+    let result;
+    try {
+        result = await checkToRegistered(inputsValues);
+    } catch(rejectObj) {
+        result = rejectObj;
+    } finally {
+        result.token? window.location.href = "./loginuserpage.html" : window.location.href = "./notautherize.html";
+    }
+}
+
+const checkToRegistered = (inputData) => {
+    return new Promise((resolve, reject) => {
+        const registeredData = {
+            userId : "abcdefg@gmail.com",
+            password : "N302aoe3"
+        }
+
+        if (inputData.userId === registeredData.userId && inputData.password === registeredData.password) {
+            resolve({ token: "fafae92rfjafa03", ok: true, code: 200 });
+        } else {
+            reject({ ok: false, code: 401 });
+        }
+    })
+}
+
+submitButton.addEventListener("click", getInputValueAndTryLogin);
