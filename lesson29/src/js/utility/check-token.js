d7c36cab9c00eac6b7d30d77bc43d9da0b5de72f
takeda haruna <h136479442@gmail.com>
2023-02-08 22:29:29 +0900

    refactor: Cutting Conditional Expressions into Functions

--- lesson29/src/js/utility/check-token.js
+++ lesson29/src/js/utility/check-token.js
@@ -1,12 +1,13 @@
 const urlParameter = Object.fromEntries(new URLSearchParams(window.location.search));
 const currentPageToken = urlParameter.token;
 const registeredToken = localStorage.getItem("token");
+const isMismatchToken = () => currentPageToken !== registeredToken;
 
 if (!registeredToken || !currentPageToken){
     window.location.href = "./../notautherize.html";
 }
 
-if (currentPageToken !== registeredToken) {
+if (isMismatchToken()) {
     localStorage.removeItem("token");
     window.location.href = "./../notautherize.html";
 }
