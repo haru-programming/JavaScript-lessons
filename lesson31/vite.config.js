f984d8198707a862770f96d87d15a49d84d440d8
Haruna Takeda <h136479442@gmail.com>
2023-03-19 23:25:40 +0900

    feat: Pass the option of the direction in which the menu will appear

--- lesson31/vite.config.js
+++ lesson31/vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 export default defineConfig({
-    root: './src',
+    root: 'src',
+    base: '/', 
     build: {
-        base: '/', 
-        outDir: '../dist',
+        outDir: './dist',
     },
 });
