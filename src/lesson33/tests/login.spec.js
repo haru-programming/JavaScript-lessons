dbea6a26a0cbc91519e54a9ce8e3117ee90c4c20
Haruna Takeda <h136479442@gmail.com>
2023-07-03 22:05:59 +0900

    refactor: Split the test into two parts

--- src/lesson33/tests/login.spec.js
+++ src/lesson33/tests/login.spec.js
@@ -1,16 +1,18 @@
 import { test, expect } from '@playwright/test';
 
-test('Pressing a link in the drawer menu closes the menu and transitions the screen', async ({ page }) => {
-    //login.htmlに遷移
-    await page.goto('http://localhost:3000/lesson33/login.html');
+test.describe('Pressing a link in the drawer menu closes the menu and transitions the screen', () => {
+    test('Pressing a link in the drawer menu', async ({ page }) => {
+      await page.goto('http://localhost:3000/lesson33/login.html');
 
-    //ハンバーガーボタンを押す
-    await page.getByRole('button', { name: 'メニューを開閉する' }).click();
-    
-    //ドロワーメニュー内の「Sign up」リンクを押す
-    await page.getByRole('link', { name: 'Sign up', exact: true }).click();
+      //ハンバーガーボタンを押す
+      await page.getByRole('button', { name: 'メニューを開閉する' }).click();
+      
+      //ドロワーメニュー内の「Sign up」リンクを押す
+      await page.getByRole('link', { name: 'Sign up', exact: true }).click();
+    });
 
-    //register.htmlに遷移
-    await page.goto('http://localhost:3000/lesson33/register.html');
-    await expect(page.getByTestId('signup-page')).toContainText('Login ▶︎ Sign Up');
+    test('closes the menu and transitions the screen', async ({ page }) => {
+      await page.goto('http://localhost:3000/lesson33/register.html');
+      await expect(page.getByTestId('signup-page')).toContainText('Login ▶︎ Sign Up');
+    });
 });
