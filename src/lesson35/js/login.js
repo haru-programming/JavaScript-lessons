1a7a8b759ee55620de5960a82e4e096fb02cbda3
Haruna Takeda <h136479442@gmail.com>
2023-11-04 21:58:13 +0900

    feat: Login function by mockAPI

--- src/lesson35/js/login.js
+++ src/lesson35/js/login.js
@@ -5,7 +5,7 @@ const chance = new Chance();
 
 const userIdOfInput = document.querySelector(".js-form-userid");
 const passwordOfInput = document.querySelector(".js-form-password");
-const formElements = [userIdOfInput,passwordOfInput];
+const formElements = [userIdOfInput, passwordOfInput];
 const eyeIcon = document.querySelector(".js-eye-icon");
 const errorOfPassword = document.querySelector('[data-name="password-error"]');
 const submitButton = document.querySelector(".js-submit-button");
@@ -15,51 +15,89 @@ formElements.forEach(element => {
     element.classList.add("invalid");
 
     element.addEventListener("blur", (e) => {
-        if(e.relatedTarget === eyeIcon) {
-            if(passwordOfInput.value && errorOfPassword.textContent === "入力してください") errorOfPassword.textContent = "";
+        if (e.relatedTarget === eyeIcon) {
+            if (passwordOfInput.value && errorOfPassword.textContent === "入力してください") errorOfPassword.textContent = "";
             return;
         }
 
         checkFormValidityInBlur(submitButton, e.target);
-        confirmIfCanSubmit(submitButton,invalidItems);
+        confirmIfCanSubmit(submitButton, invalidItems);
     });
 });
 
 eyeIcon.addEventListener("click", togglePasswordDisplay);
 eyeIcon.addEventListener("blur", (e) => {
-    if(e.relatedTarget === passwordOfInput) return;
+    if (e.relatedTarget === passwordOfInput) return;
     checkFormValidityInBlur(submitButton, passwordOfInput);
 
-    if(invalidItems > 0) return;
-    confirmIfCanSubmit(submitButton,invalidItems);
+    if (invalidItems > 0) return;
+    confirmIfCanSubmit(submitButton, invalidItems);
 });
 
-const tryToLogin = async() => {
-    let result;
+const url = "https://652dc444f9afa8ef4b27cca3.mockapi.io/users";
+
+
+const displayInfo = (target, error) => {
+    const p = document.createElement("p");
+    p.textContent = error;
+    target.appendChild(p);
+};
+
+const displayErrorStatus = (target, response) => {
+    const p = document.createElement("p");
+    p.textContent = `${response.status}:${response.statusText}`;
+    target.appendChild(p);
+};
+
+const fetchData = async (api) => {
     try {
-        result = await checkToRegistered();
-        localStorage.setItem("token", result.token);
-    } catch(rejectObj) {
-        result = rejectObj;
-    } finally {
-        window.location.href = result.token ? "./index.html" : "./notautherize.html";
+        const response = await fetch(api);
+
+        if (response.ok) {
+            return await response.json();
+        } else {
+            console.error(`${response.status}:${response.statusText}`);
+            displayErrorStatus(errorArea, response);
+        }
+
+    } catch (error) {
+        displayInfo(errorArea, error);
+    }
+};
+
+const fetchRegisteredData = async () => {
+    const data = await fetchData(url);
+    if (!data) return;
+
+    if (data.length) {
+        return data;
+    }
+};
+
+const checkToRegistered = async (userIdInput, passwordInput) => {
+    const registeredUsers = await fetchRegisteredData();
+    const user = registeredUsers.find(user => user.name === userIdInput || user.email === userIdInput);
+
+    if (user && user.password === passwordInput) {
+        return { token: user.userId, ok: true, code: 200 };
+    } else {
+        throw { ok: false, code: 401 };
     }
 }
 
-const checkToRegistered = () => {
-    return new Promise((resolve, reject) => {
-        const inputsValues = {
-            userId: userIdOfInput.value,
-            password: passwordOfInput.value
-        }
-        const registeredData = JSON.parse(localStorage.getItem("registeredData"));
+const tryToLogin = async () => {
+    const userIdInput = document.getElementById('userId').value;
+    const passwordInput = document.getElementById('password').value;
 
-        if ((inputsValues.userId === registeredData.name || inputsValues.userId === registeredData.email) && (inputsValues.password === registeredData.password)) {
-            resolve({ token: chance.apple_token(), ok: true, code: 200 });
-        } else {
-            reject({ ok: false, code: 401 });
-        }
-    })
+    try {
+        const result = await checkToRegistered(userIdInput, passwordInput);
+        localStorage.setItem("token", result.token);
+        window.location.href = "./logged-in.html";
+    } catch (rejectObj) {
+        console.error('Login failed:', rejectObj);
+        window.location.href = "./notautherize.html";
+    }
 }
+
 
 submitButton.addEventListener("click", tryToLogin);
